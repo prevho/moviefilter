17b024ae357f29dbe3834acda56e313e51323af5
prevho <precious.iriaevho@gmail.com>
2022-05-24 04:18:05 +0100

    MovieFilter001

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from "react";
+import { useState, useEffect } from "react";
 import './App.css';
+import searchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// http://www.omdbapi.com/?i=tt3896198&apikey=c6ffb478
+
+const API_URL = "http://www.omdbapi.com/?apikey=c6ffb478";
+
+// const movie1 = {
+//     "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+//     "Year": "2007",
+//     "imdbID": "tt1132238",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
+// }
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        // console.log(data.Search);
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies('spiderman');
+    }, []);
+
+    return (
+        <div className="app">
+          <h1>MovieFilter</h1>
+    
+          <div className="search">
+            <input
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="Search for movies"
+            />
+            <img
+              src={searchIcon}
+              alt="search"
+              onClick={() => searchMovies(searchTerm)}
+            />
+          </div>
+    
+          {movies?.length > 0 ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )}
+        </div>
+      );
+    };
 
 export default App;
---
+++ srcDELETED/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+// function App() {
+const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{
+    setCounter(100);
+  }, []);
+
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((nextCount) => nextCount + 1)}>+</button>
+    </div>
+  );
+}
+
+export default App;
